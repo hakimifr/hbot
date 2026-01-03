bfaf3926ee987dc37814e8cbc8856eb22bb33128
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2026-01-03 16:04:05 +0800

    feat: implement gemini plugin
    Co-authored-by: kamski18 <addinzafran1505@gmail.com>

---
+++ hbot/plugins/gemini.py
@@ -0,0 +1,56 @@
+import logging
+import os
+
+from google import genai
+from google.genai import types
+from pyrogram import filters
+from pyrogram.client import Client
+from pyrogram.handlers.handler import Handler
+from pyrogram.handlers.message_handler import MessageHandler
+
+from hbot.base_plugin import BasePlugin
+
+logger = logging.getLogger(__name__)
+
+
+class Gemini(BasePlugin):
+    name: str = "Gemini Plugin"
+    description: str = "Plugin for Gemini"
+
+    def __init__(self, app: Client) -> None:
+        self.app: Client = app
+
+    async def search_handler(self, client, message):
+        parts = message.text.split(maxsplit=1)
+        if len(parts) > 1:
+            prompt = parts[1]
+            await message.edit("Asking..")
+            try:
+                # Call the async helper
+                response_text = await self.ask_gemini(prompt)
+                await message.edit(response_text)
+            except TimeoutError as e:
+                logging.error(f"Gemini Error: {e}")
+                await message.edit(f"Error: {str(e)}")
+        else:
+            await message.edit("Please provide a search query!")
+
+    async def ask_gemini(self, text_to_be_ask) -> str:
+        api_key = os.getenv(key="GEMINI_API_KEY")
+        client = genai.Client(api_key=api_key)
+        config = types.GenerateContentConfig(temperature=1.0)
+
+        try:
+            response = await client.aio.models.generate_content(
+                model="gemini-2.5-flash", contents=text_to_be_ask, config=config
+            )
+            if response.text is None:
+                raise
+        except Exception:
+            logger.error("error when generating response, traceback:", exc_info=True)
+            return "error when generating response, see log for more info"
+
+        return response.text
+
+    def register_handlers(self) -> list[Handler]:
+        return [MessageHandler(self.ask_gemini, filters.command("ask", prefixes=self.prefixes) & filters.me)]
